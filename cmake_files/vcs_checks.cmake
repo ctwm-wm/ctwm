6a9dd76ff95064960ebf1ed0129fadece0ca132f
Matthew D. Fuller <fullermd@over-yonder.net>
2025-07-30 21:01:32 -0500

    Rework the VCS check stuff to assume git as the default, and be a bit more structured to add more fallbacks if it ever becomes necessary.

--- cmake_files/vcs_checks.cmake
+++ cmake_files/vcs_checks.cmake
@@ -3,9 +3,20 @@
 #
 
 
-# See if we're building from a bzr checkout.  This is fragile in the
-# sense that it'll break if the bzr WT format changes, but that's
-# staggeringly unlikely now, so...
+# See if we're building in a git repo.  This is the presumed current
+# world.
+#
+# Assume .git/ stays stable in format so this works...
+set(GIT_INDEX_FILE ${CMAKE_SOURCE_DIR}/.git/index)
+set(IS_GIT_CO 0)
+if(EXISTS ${GIT_INDEX_FILE})
+	set(IS_GIT_CO 1)
+endif()
+
+# See if we're building from a bzr checkout.
+#
+# This is fragile in the sense that it'll break if the bzr WT format
+# changes, but that's staggeringly unlikely now, so...
 set(BZR_DIRSTATE_FILE ${CMAKE_SOURCE_DIR}/.bzr/checkout/dirstate)
 if(EXISTS ${BZR_DIRSTATE_FILE})
 	set(IS_BZR_CO 1)
@@ -14,40 +25,37 @@ else()
 endif()
 
 
-# If we are, see if we can find bzr(1) installed
+
+# OK, did we get any VCSy stuff?
+set(DID_VCS 0)
+
+
+# Are we git?  Be gittish.
+set(HAS_GIT 0)
+if(NOT DID_VCS AND IS_GIT_CO)
+	find_program(GIT_CMD git)
+	if(GIT_CMD)
+		set(HAS_GIT 1)
+		set(DID_VCS 1)
+		message(STATUS "Building from git repo and found git (${GIT_CMD}).")
+	else()
+		message(STATUS "Building from git repo, but no git found.")
+	endif(GIT_CMD)
+endif() # git
+
+
+# Do we fallback to bzr?
 set(HAS_BZR 0)
-if(IS_BZR_CO)
+if(NOT DID_VCS AND IS_BZR_CO)
 	find_program(BZR_CMD NAMES bzr brz)
 	if(BZR_CMD)
 		set(HAS_BZR 1)
+		set(DID_VCS 1)
 		message(STATUS "Building from a checkout and found bzr (${BZR_CMD}).")
 	else()
 		message(STATUS "Building from a checkout, but no bzr found.")
 	endif(BZR_CMD)
-else()
-	message(STATUS "You aren't building from a bzr checkout.")
-endif(IS_BZR_CO)
-
-
-# If not bzr, do a little check to see if we're building from git instead
-if(NOT IS_BZR_CO)
-	set(GIT_INDEX_FILE ${CMAKE_SOURCE_DIR}/.git/index)
-	set(IS_GIT_CO 0)
-	if(EXISTS ${GIT_INDEX_FILE})
-		set(IS_GIT_CO 1)
-	endif()
-
-	if(IS_GIT_CO)
-		set(HAS_GIT 0)
-		find_program(GIT_CMD git)
-		if(GIT_CMD)
-			set(HAS_GIT 1)
-			message(STATUS "Building from git repo and found git (${GIT_CMD}).")
-		else()
-			message(STATUS "Building from git repo, but no git found.")
-		endif(GIT_CMD)
-	endif(IS_GIT_CO)
-endif(NOT IS_BZR_CO)
+endif() # bzr
 
 
 # Flag for dev use
